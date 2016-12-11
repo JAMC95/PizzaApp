a47f31048e859625f12790a5d520bf3fdcb721a7
Jose Antonio Moral Castilla <joxe.bailen@gmail.com>
2016-12-11 16:28:24 +0100

    Creada ruta para login

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
-
+router.get('/login', function(req, res) {
+  res.render('login');
+});
 module.exports = router;
