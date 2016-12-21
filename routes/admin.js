2f6157a1719371d1d849c3d55c522fda0f2fc5b2
Jose Antonio Moral Castilla <joxe.bailen@gmail.com>
2016-12-21 16:56:23 +0100

    Punto y coma faltantne admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,7 @@ router.get('/', ensureAuthenticated, function(req, res) {
   .exec(function(err, orders) {
     if(err) throw err;
     res.render('admin', {orders: orders});
-  })
+  });
 
-})
+});
 module.exports = router;
