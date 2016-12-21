27396bb6bae50f493200dffd8db8d2f3b750f0f3
Jose Antonio Moral Castilla <joxe.bailen@gmail.com>
2016-12-21 15:29:01 +0100

    Comrpobamos que el usuario esté logeado para hacer un pedido

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
-router.get('/', function(req, res) {
+function ensureAuthenticated(req, res, next) {
+  if(req.isAuthenticated()) return next();
+  return res.redirect('/user/login');
+}
+router.get('/', ensureAuthenticated, function(req, res) {
   res.render('order', {user: req.user});
 });
 
